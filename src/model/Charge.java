7b6dba109ecbf4a07112f9a03b67633fd2d2faa5
40CompilerErrors <danvgdesign@gmail.com>
2017-06-17 21:50:05 +0200

    Add files via upload

--- src/model/Charge.java
+++ src/model/Charge.java
@@ -6,8 +6,11 @@
 package model;
 
 /**
- *
- * @author Daniel
+ * Data representation of an electrical charge, including position and value.
+ * 
+ * @author ALTF4
+ * @version 1.1
+ * @see control.MainController
  */
 public class Charge {
     
@@ -15,7 +18,12 @@ public class Charge {
     private double y;
     private double value;
     
-    
+    /**
+    * Constructor without position (Set on 0,0)
+    * 
+    * @param double v
+    * @return Charge
+    */
     public Charge(double v){
         
         this.x = 0;
@@ -23,7 +31,12 @@ public class Charge {
         this.value = v;      
     }   
     
-    
+    /**
+    * Constructor for the class.
+    * 
+    * @param double x, double y, double v
+    * @return Charge
+    */
     public Charge(double x, double y, double v){
         
         this.x = x;
@@ -31,19 +44,43 @@ public class Charge {
         this.value = v;        
     }   
     
+    /**
+    * Setter for the X axis.
+    * 
+    * @param double x
+    * @return nothing
+    */
     public double getX(){       
         return x;     
     }
     
+    /**
+    * Setter for the Y axis
+    * 
+    * @param double y
+    * @return nothing
+    */
     public double getY(){       
         return y;     
     }
     
+    /**
+    * Getter for the charge's value in microCoulombs.
+    * 
+    * @param nothing
+    * @return double value
+    */
     public double getVal(){       
         return value;     
         
     }
     
+    /**
+    * Setter for the charge's value in microCoulombs.
+    * 
+    * @param double v
+    * @return nothing
+    */
     public void setVal(double v){       
         this.value = v;          
     }
